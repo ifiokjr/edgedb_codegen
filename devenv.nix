23aaafad52e22307abbbc88bfcd3c8e3f6fda99f
Ifiok Jr. <ifiokotung@gmail.com>
2024-08-27 12:13:10 +0100

    ci: remove unused tools and packages

--- devenv.nix
+++ devenv.nix
@@ -4,17 +4,14 @@
   packages = [
     pkgs.cargo-binstall
     pkgs.cargo-run-bin
-    pkgs.coreutils
     pkgs.dprint
     pkgs.edgedb
-    pkgs.nil
-    pkgs.nixpkgs-fmt
+    pkgs.nixfmt
     pkgs.rustup
     pkgs.shfmt
-    pkgs.taplo
-    pkgs.yamlfmt
   ] ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk; [
     pkgs.libiconv
+    pkgs.coreutils
     frameworks.Security
     frameworks.System
   ]);
@@ -116,23 +113,5 @@
     echo "DEVENV_PROFILE=$DEVENV_PROFILE" >> $GITHUB_ENV
     echo "DEVENV_ROOT=$DEVENV_ROOT" >> $GITHUB_ENV
     echo "DEVENV_STATE=$DEVENV_STATE" >> $GITHUB_ENV
-  '';
-  scripts."setup:docker".exec = ''
-    set -e
-    # update path
-    echo "export PATH=$DEVENV_PROFILE/bin:\$PATH" >> /etc/profile
-
-    echo "export DEVENV_PROFILE=$DEVENV_PROFILE" >> /etc/profile
-    echo "export PKG_CONFIG_PATH=$PKG_CONFIG_PATH" >> /etc/profile
-    echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> /etc/profile
-    echo "export LIBRARY_PATH=$LIBRARY_PATH" >> /etc/profile
-    echo "export C_INCLUDE_PATH=$C_INCLUDE_PATH" >> /etc/profile
-    echo "export XDG_DATA_DIRS=$XDG_DATA_DIRS" >> /etc/profile
-    echo "export XDG_CONFIG_DIRS=$XDG_CONFIG_DIRS" >> /etc/profile
-
-    echo "export DEVENV_DOTFILE=$DEVENV_DOTFILE" >> /etc/profile
-    echo "export DEVENV_PROFILE=$DEVENV_PROFILE" >> /etc/profile
-    echo "export DEVENV_ROOT=$DEVENV_ROOT" >> /etc/profile
-    echo "export DEVENV_STATE=$DEVENV_STATE" >> /etc/profile
   '';
 }
