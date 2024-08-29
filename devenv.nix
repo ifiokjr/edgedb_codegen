fd77215205e6fd6cfe11f2201bd39e89b5b5c6a7
Ifiok Jr. <ifiokotung@gmail.com>
2024-08-29 19:37:49 +0100

    ci: add `build:docs` check

--- devenv.nix
+++ devenv.nix
@@ -1,119 +1,193 @@
 { pkgs, lib, ... }:
 
 {
-  packages = [
-    pkgs.cargo-binstall
-    pkgs.cargo-run-bin
-    pkgs.dprint
-    pkgs.edgedb
-    pkgs.nixfmt
-    pkgs.rustup
-    pkgs.shfmt
-  ] ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk; [
-    pkgs.libiconv
-    pkgs.coreutils
-    frameworks.Security
-    frameworks.System
-  ]);
+  packages =
+    [
+      pkgs.cargo-binstall
+      pkgs.cargo-run-bin
+      pkgs.dprint
+      pkgs.edgedb
+      pkgs.nixfmt-rfc-style
+      pkgs.rustup
+      pkgs.shfmt
+    ]
+    ++ lib.optionals pkgs.stdenv.isDarwin (
+      with pkgs.darwin.apple_sdk;
+      [
+        pkgs.libiconv
+        pkgs.coreutils
+        frameworks.Security
+        frameworks.System
+      ]
+    );
 
   # disable dotenv since it breaks the variable interpolation supported by `direnv`
   dotenv.disableHint = true;
 
-  scripts."install:all".exec = ''
-    set -e
-    install:cargo:bin
-  '';
-  scripts."install:cargo:bin".exec = ''
-    cargo bin --install
-  '';
-  scripts."db:destroy".exec = ''
-    set -e
-    edgedb instance destroy -I $EDGEDB_INSTANCE --non-interactive --force
-  '';
-  scripts."db:setup".exec = ''
-    set -e
-    edgedb instance create --non-interactive $EDGEDB_INSTANCE $EDGEDB_BRANCH || true
-    edgedb instance start --instance $EDGEDB_INSTANCE
-    edgedb migrate
-  '';
-  scripts."db:up".exec = ''
-    set -e
-    edgedb watch --instance $EDGEDB_INSTANCE
-  '';
-  scripts."update:deps".exec = ''
-    set -e
-    cargo update
-  '';
-  scripts."fix:all".exec = ''
-    set -e
-    fix:clippy
-    fix:format
-    cargo deny check
-  '';
-  scripts."fix:format".exec = ''
-    set -e
-    dprint fmt --config "$DEVENV_ROOT/dprint.json"
-  '';
-  scripts."fix:clippy".exec = ''
-    set -e
-    cargo clippy --fix --allow-dirty --allow-staged --all-features
-  '';
-  scripts."lint:all".exec = ''
-    set -e
-    lint:clippy
-    lint:format
-    cargo deny check
-  '';
-  scripts."lint:format".exec = ''
-    set -e
-    dprint check
-  '';
-  scripts."lint:clippy".exec = ''
-    set -e
-    cargo clippy --all-features
-  '';
-  scripts."test:all".exec = ''
-    set -e
-    cargo test_codegen
-    cargo test_docs
-  '';
-  scripts."coverage:all".exec = ''
-    set -e
-    cargo coverage_codegen
-    cargo coverage_docs
-    cargo coverage_report
-  '';
-  scripts."setup:vscode".exec = ''
-    set -e
-    rm -rf .vscode
-    cp -r $DEVENV_ROOT/setup/editors/vscode .vscode
-  '';
-  scripts."setup:helix".exec = ''
-    set -e
-    rm -rf .helix
-    cp -r $DEVENV_ROOT/setup/editors/helix .helix
-  '';
-  scripts."setup:ci".exec = ''
-    set -e
-    # update github ci path
-    echo "$DEVENV_PROFILE/bin" >> $GITHUB_PATH
+  scripts."install:all" = {
+    exec = ''
+      set -e
+      install:cargo:bin
+    '';
+    description = "Install all dependencies.";
+  };
+  scripts."install:cargo:bin" = {
+    exec = ''
+      cargo bin --install
+    '';
+    description = "Install cargo binaries locally.";
+  };
+  scripts."db:destroy" = {
+    exec = ''
+      set -e
+      edgedb instance destroy -I $EDGEDB_INSTANCE --non-interactive --force
+    '';
+    description = "Destroy the local database.";
+  };
+  scripts."db:setup" = {
+    exec = ''
+      set -e
 
-    # update github ci environment
-    echo "DEVENV_PROFILE=$DEVENV_PROFILE" >> $GITHUB_ENV
+      if [ ! -f "$DEVENV_ROOT/.env" ]; then
+        cp $DEVENV_ROOT/.env.example $DEVENV_ROOT/.env
+        export $(cat .env | xargs)
+      fi
 
-    # prepend common compilation lookup paths
-    echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH" >> $GITHUB_ENV
-    echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> $GITHUB_ENV
-    echo "LIBRARY_PATH=$LIBRARY_PATH" >> $GITHUB_ENV
-    echo "C_INCLUDE_PATH=$C_INCLUDE_PATH" >> $GITHUB_ENV
+      edgedb instance create --non-interactive $EDGEDB_INSTANCE $EDGEDB_BRANCH || true
+      edgedb instance start --instance $EDGEDB_INSTANCE
+      edgedb migrate
+    '';
+    description = "Setup the local database.";
+  };
+  scripts."db:up" = {
+    exec = ''
+      set -e
+      edgedb watch --instance $EDGEDB_INSTANCE
+    '';
+    description = "Watch changes to the local database.";
+  };
+  scripts."update:deps" = {
+    exec = ''
+      set -e
+      cargo update
+    '';
+    description = "Update dependencies.";
+  };
+  scripts."build:all" = {
+    exec = ''
+      cargo build --all-features
+    '';
+    description = "Build all crates with all features activated.";
+  };
+  scripts."build:docs" = {
+    exec = ''
+      RUSTDOCFLAGS="--cfg docsrs" cargo doc --all-features
+    '';
+    description = "Build documentation site.";
+  };
+  scripts."fix:all" = {
+    exec = ''
+      set -e
+      fix:clippy
+      fix:format
+      cargo deny check
+    '';
+    description = "Fix all fixable lint issues.";
+  };
+  scripts."fix:format" = {
+    exec = ''
+      set -e
+      dprint fmt --config "$DEVENV_ROOT/dprint.json"
+    '';
+    description = "Fix formatting for entire project.";
+  };
+  scripts."fix:clippy" = {
+    exec = ''
+      set -e
+      cargo clippy --fix --allow-dirty --allow-staged --all-features
+    '';
+    description = "Fix fixable lint issues raised by rust clippy.";
+  };
+  scripts."lint:all" = {
+    exec = ''
+      set -e
+      lint:clippy
+      lint:format
+      cargo deny check
+    '';
+    description = "Lint all project files.";
+  };
+  scripts."lint:format" = {
+    exec = ''
+      set -e
+      dprint check
+    '';
+    description = "Check all formatting is correct.";
+  };
+  scripts."lint:clippy" = {
+    exec = ''
+      set -e
+      cargo clippy --all-features
+    '';
+    description = "Check rust clippy lints.";
+  };
+  scripts."test:all" = {
+    exec = ''
+      set -e
+      cargo test_codegen
+      cargo test_docs
+    '';
+    description = "Test all project files.";
+  };
+  scripts."coverage:all" = {
+    exec = ''
+      set -e
+      cargo coverage_codegen
+      cargo coverage_docs
+      cargo coverage_report
+    '';
+    description = "Test all files and generate a coverage report for upload to codecov.";
+  };
+  scripts."setup:vscode" = {
+    exec = ''
+      set -e
+      rm -rf .vscode
+      cp -r $DEVENV_ROOT/setup/editors/vscode .vscode
+    '';
+    description = "Setup the vscode editor for development.";
+  };
+  scripts."setup:helix" = {
+    exec = ''
+      set -e
+      rm -rf .helix
+      cp -r $DEVENV_ROOT/setup/editors/helix .helix
+    '';
+    description = "Setup the helix editor for development.";
+  };
+  scripts."setup:ci" = {
+    exec = ''
+      set -e
+      # update github ci path
+      echo "$DEVENV_PROFILE/bin" >> $GITHUB_PATH
 
-    # these provide shell completions / default config options
-    echo "XDG_DATA_DIRS=$XDG_DATA_DIRS" >> $GITHUB_ENV
-    echo "XDG_CONFIG_DIRS=$XDG_CONFIG_DIRS" >> $GITHUB_ENV
+      # update github ci environment
+      echo "DEVENV_PROFILE=$DEVENV_PROFILE" >> $GITHUB_ENV
 
-    echo "DEVENV_DOTFILE=$DEVENV_DOTFILE" >> $GITHUB_ENV
-    echo "DEVENV_PROFILE=$DEVENV_PROFILE" >> $GITHUB_ENV
-    echo "DEVENV_ROOT=$DEVENV_ROOT" >> $GITHUB_ENV
-    echo "DEVENV_STATE=$DEVENV_STATE" >> $GITHUB_ENV
-  '';
+      # prepend common compilation lookup paths
+      echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH" >> $GITHUB_ENV
+      echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> $GITHUB_ENV
+      echo "LIBRARY_PATH=$LIBRARY_PATH" >> $GITHUB_ENV
+      echo "C_INCLUDE_PATH=$C_INCLUDE_PATH" >> $GITHUB_ENV
+
+      # these provide shell completions / default config options
+      echo "XDG_DATA_DIRS=$XDG_DATA_DIRS" >> $GITHUB_ENV
+      echo "XDG_CONFIG_DIRS=$XDG_CONFIG_DIRS" >> $GITHUB_ENV
+
+      echo "DEVENV_DOTFILE=$DEVENV_DOTFILE" >> $GITHUB_ENV
+      echo "DEVENV_PROFILE=$DEVENV_PROFILE" >> $GITHUB_ENV
+      echo "DEVENV_ROOT=$DEVENV_ROOT" >> $GITHUB_ENV
+      echo "DEVENV_STATE=$DEVENV_STATE" >> $GITHUB_ENV
+    '';
+    description = "Setup the github ci environment.";
+  };
 }
